f3c877014778a23c4de551bceae79da3b23c1e41
OneScriptMan <vladzam79@gmail.com>
2023-10-17 18:42:09 +0300

    Add listeners to all counters

--- js/counter.js
+++ js/counter.js
@@ -1,16 +1,36 @@
-const plus = document.querySelector('#plus');
-const minus = document.querySelector('#minus');
-const result = document.querySelector('#result-counter')
+document.body.addEventListener('click', (event) => {
 
-plus.addEventListener('click', () => {  
-  result.innerHTML++
-})
+  if (event.target.dataset.action === 'plus') {
+   const currentCounter = event.target.closest('.js-counter');
 
-minus.addEventListener('click', () => {
-  currentResult = Number(result.innerHTML);
-  if (currentResult === 0){
-    return
+   currentCounter.querySelector('#result-counter').innerHTML++
+
   }
-  else {
-  result.innerHTML--};
+  else if (event.target.dataset.action === 'minus') {
+    const currentCounter = event.target.closest('.js-counter');
+    let counterInner = currentCounter.querySelector('#result-counter');
+
+    if (Number(counterInner.innerHTML) > 0) {      
+      counterInner.innerHTML--;
+    }   
+  }
 })
+
+
+
+// const plus = document.querySelector('#plus');
+// const minus = document.querySelector('#minus');
+// const result = document.querySelector('#result-counter')
+
+// plus.addEventListener('click', () => {  
+//   result.innerHTML++
+// })
+
+// minus.addEventListener('click', () => {
+//   currentResult = Number(result.innerHTML);
+//   if (currentResult === 0){
+//     return
+//   }
+//   else {
+//   result.innerHTML--};
+// })
